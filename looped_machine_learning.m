f5cab850872bb0e7a8848644a7159556ff98633c
bengiugliano <bgiugliano6@gmail.com>
2016-07-07 15:48:13 -0400

    Update looped_machine_learning.m

--- looped_machine_learning.m
+++ looped_machine_learning.m
@@ -7,24 +7,25 @@ labelID = fopen('/Users/bengiugliano/Downloads/train-labels-2.idx1-ubyte');
 LabelVector = fread(labelID);
 %Find all places in the label array where there is a 0 or 1, and place in
 %an array.
-SVMModel = zeros(9,1);
+check = 0;
+runlength = 5421;
 for count = 0: 1: 8
-   for num = count+1: 9
+   for num = (count+1): 9
        numberSmall = find(LabelVector==count);
-       numberSmallLabels = zeros(length(numberSmall),1);
-       for k = 1:length(numberSmall)
+       numberSmallLabels = zeros(runlength,1);
+       for k = 1:runlength
          numberSmallLabels(k,1) = count;
        end
        numberLarge = find(LabelVector==num);
-       numberLargeLabels = zeros(length(numberLarge),1);
-       for k = 1:length(numberLarge)
+       numberLargeLabels = zeros(runlength,1);
+       for k = 1:runlength
          numberLargeLabels(k,1) = num;
        end
        numberSmall = numberSmall - 8;
        numberLarge = numberLarge - 8;
        % get small and large number ^
-       smallArray = zeros(784, length(numberSmall));
-       for i=1: length(numberSmall)
+       smallArray = zeros(784, runlength);
+       for i=1: runlength
            if numberSmall(i)>0
                start=16+((numberSmall(i)-1)*784);
                stop=16+(numberSmall(i)*784);
@@ -37,8 +38,8 @@ for count = 0: 1: 8
        start(:) =[];
        stop(:)=[];
        %%%%%%%%%%% large num images
-       largeArray = zeros(784, length(numberLarge));
-       for i=1: length(numberLarge)
+       largeArray = zeros(784, runlength);
+       for i=1: runlength
            if numberLarge(i)>0
                start=16+((numberLarge(i)-1)*784);
                stop=16+(numberLarge(i)*784);
@@ -50,12 +51,14 @@ for count = 0: 1: 8
        largeArray = transpose(largeArray);
        start(:) =[];
        stop(:)=[];
-       
        %%%%%%%%%% SVM stuff
        combinedImages = [smallArray;largeArray];
-       combinedLabels = [numberSmall;numberLarge];
+       combinedLabels = [numberSmallLabels;numberLargeLabels];
        %combinedLabelsGood = transpose(combinedLabels);
-       SVMModel((count+1),1) = fitcsvm(combinedImages,combinedLabels,'KernelFunction','linear','Standardize',true);
+       check = check + 1
+       s = 'SVMModel';
+       interim = fitcsvm(combinedImages,combinedLabels,'KernelFunction','linear','Standardize',true);
+       strcat(s,num2str(check))
        combinedImages(:) =[];
        combinedLabels(:) =[];
    end
